cdbeb9d06279f9cb3bfc1f110457dd55cf27ab78
Nnstein <akachinnstein@gmail.com>
2023-06-24 11:27:29 +0100

    add updated langchain to requirement

--- api.py
+++ api.py
@@ -3,5 +3,5 @@
 # log = pd.read_csv('langchain\credentials.csv')
 # apikey = 'log['value'][10]'
 
-apikey = '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'
+apikey = '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'
 # serpapikey = 'be15986939837fad631e9930b5b32202d529ce19ea925deb99d8545cd49b44f1'
